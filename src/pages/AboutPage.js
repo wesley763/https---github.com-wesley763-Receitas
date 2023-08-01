0ce64fbc4fad3e8f16626758dce3d4e1f09636cf
wesley <magalhaesw763@gmail.com>
2023-08-01 17:02:40 -0300

    commit 1

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container } from '../styles';
+import './AboutPage.css';
+
+
+const AboutPage = () => {
+  return (
+    <Container>
+      <h2>Sobre</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin gravida
+        felis sit amet orci cursus, eget feugiat felis convallis. Sed facilisis
+        sapien vitae purus congue, et bibendum sapien tincidunt. Nam euismod
+        tristique metus, a semper eros lacinia ac. Nullam eu enim vitae mauris
+        varius volutpat.
+      </p>
+      <p>
+        Sed eu erat ex. Nulla ultrices nisl a elit posuere, et iaculis enim
+        tincidunt. Integer ultrices ligula sit amet risus rutrum, non bibendum
+        orci vehicula. Nulla facilisi. Sed aliquam vestibulum nunc at blandit.
+        Fusce quis ipsum sit amet quam congue scelerisque. Proin tincidunt,
+        augue non iaculis venenatis, ligula turpis mattis nisi, vitae finibus
+        arcu dui vel massa. In vulputate ipsum eget felis ultrices, in
+        tristique quam consectetur. Nunc id feugiat magna, eget consectetur
+        ipsum. Duis malesuada arcu eget diam luctus, eget cursus nisl semper.
+        Sed mattis leo at quam iaculis venenatis.
+      </p>
+      <p>
+        Aenean scelerisque, quam non finibus pharetra, ex felis ullamcorper
+        justo, vel interdum velit arcu nec ex. Nullam sed mi et erat iaculis
+        varius nec ut nunc. Pellentesque habitant morbi tristique senectus et
+        netus et malesuada fames ac turpis egestas. Duis nec libero eget velit
+        bibendum venenatis vitae at felis. Vivamus id metus purus. Fusce eu
+        venenatis enim, eu vestibulum dui. Integer vitae nunc ut metus maximus
+        consectetur. Sed at velit eu justo vehicula dignissim.
+      </p>
+    </Container>
+  );
+};
+
+export default AboutPage;
