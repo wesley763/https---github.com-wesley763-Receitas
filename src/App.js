08ec5f79b1d28c27c02d7a89252afba71a7fa769
wesley <magalhaesw763@gmail.com>
2023-08-03 14:39:26 -0300

    Add Cep

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Menu from './components/Menu';
 import Home from './pages/Home';
 import Recipes from './pages/Recipes';
 import RecipeDetailPage from './pages/RecipeDetailPage';
